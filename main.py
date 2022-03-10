dbd6845533da91030f36e245334c856748a66aa1
Daniel James <86825914+attlka@users.noreply.github.com>
2022-03-10 22:42:02 +0000

    staged sample document

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import os
 import openai
 openai.api_key = os.getenv("gptkey")
+
+openai.File.create(file=open("sampledoc.pdf"), purpose='answers')
+
 openai.Answer.create(
-  search_model="ada",
-  model="babbage",
-  question="What are the branches of philosophy?",
+  search_model="davinci",
+  model="davinci",
+  question="What is the primary investigation of ethics?",
   documents=["sampledoc.pdf"],
-	examples_context="Babylonian Philosophy is an example of bilingual intellectual culture."
-	examples=[["What is an example of bilingual intellectual culture?","Babylonian Philosophy."]],
+  examples_context="Babylonian Philosophy is an example of bilingual intellectual culture.",
+  examples=[["What is an example of bilingual intellectual culture?","Babylonian Philosophy."]],
   max_tokens=70,
   stop=["\n", "<|endoftext|>"],
 )
